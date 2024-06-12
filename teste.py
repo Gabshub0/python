8a3d2fc26968ed43930c56663b58a1078564efcc
bibi <gabriellysweet0@gmail.com>
2024-06-12 14:03:20 -0300

    Create teste.py

---
+++ teste.py
@@ -0,0 +1,25 @@
+adicao = 10+10
+print("Adicao", adicao)
+
+subtracao = 10-5
+print('Subtração', subtracao)
+
+mutiplicacao = 10*10
+print("Multiplicação", mutiplicacao)
+
+divisao = 10 / 3 # float
+print("Divisão", divisao)
+divisao_inteira = 10 // 3
+print('Divisao inteira', divisao_inteira)
+
+exponeciacao = 2 ** 10
+print("Exponeciacao", exponeciacao)
+
+modulo = 55 % 2 # resto da divisao
+print('Modulo', modulo)
+
+print(10 % 8 == 0)
+print(16 % 8 == 0)
+print(10 % 2 == 0)
+print(15 % 2 == 0)
+print(16 % 2 == 0)
